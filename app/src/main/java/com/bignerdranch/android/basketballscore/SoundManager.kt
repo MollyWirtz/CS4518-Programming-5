259be1cac76d71d45fca2773fb15d50ab292c8ce
mollywirtz <mmwirtz@wpi.edu>
2020-09-30 11:56:24 -0400

    Play audio with SoundPool

---
+++ app/src/main/java/com/bignerdranch/android/basketballscore/SoundManager.kt
@@ -0,0 +1,71 @@
+package com.bignerdranch.android.basketballscore
+
+import android.content.res.AssetFileDescriptor
+import android.content.res.AssetManager
+import android.media.SoundPool
+import android.util.Log
+import java.lang.Exception
+
+private const val TAG = "SoundManager"
+private const val SOUNDS_FOLDER = "sample_sounds"
+private const val MAX_SOUNDS = 2
+
+class SoundManager (private val assets: AssetManager) {
+
+    val sounds: List<Sound>
+    private val soundPool = SoundPool.Builder()
+        .setMaxStreams(MAX_SOUNDS)
+        .build()
+
+    init {
+        sounds = loadSounds()
+    }
+
+    fun play (sound: Sound) {
+        sound.soundId?.let {
+            soundPool.play(it,1.0f, 1.0f, 1, 0, 1.0f)
+        }
+    }
+
+    fun playSound (identifier: String) {
+        if (identifier == "A") {
+            play(sounds[0])
+        } else {
+            play(sounds[1])
+        }
+    }
+
+    fun loadSounds(): List<Sound> {
+
+        val soundNames: Array<String>
+
+        try {
+            soundNames = assets.list(SOUNDS_FOLDER)!!
+        } catch (e: Exception) {
+            Log.e(TAG, "Could not list assets", e)
+            return emptyList()
+        }
+
+        val sounds = mutableListOf<Sound>()
+        soundNames.forEach { filename ->
+            val assetPath = "$SOUNDS_FOLDER/$filename"
+            val sound = Sound(assetPath)
+            try {
+                load(sound)
+                sounds.add(sound)
+            } catch (ioe: java.io.IOException) {
+                Log.e(TAG, "Could not load sound $filename", ioe)
+            }
+        }
+        return sounds
+    }
+
+
+    private fun load (sound: Sound) {
+        val afd: AssetFileDescriptor = assets.openFd(sound.assetPath)
+        val soundId = soundPool.load(afd, 1)
+        sound.soundId = soundId
+    }
+
+
+}
